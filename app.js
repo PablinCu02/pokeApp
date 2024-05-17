8ed6a1452fa63558089f2ad75d0546d5d9da3918
Pablo Gonzalez Cuellar <bacoon0202@gmail.com>
2024-05-16 21:43:12 -0600

    PokeApp Finalizada

--- app.js
+++ app.js
@@ -8,12 +8,11 @@ const imgPoke = document.querySelector('#poke');
 const namePoke= document.querySelector('#nombrePoke-propio');
 const pokeTipo= document.querySelector('#tipoPropio');
 const pokeAtaque = document.querySelector('#ataquePropio');
-const btnPelar =document.querySelector('#combate');
+const btnPelear =document.querySelector('#btn-combate');
 
 const input=document.querySelector('#input');
 const btnElegir=document.querySelector('#btn-poke');
 
-const btnPelear  = document.querySelector('#combate');
 
 
 const getNumRandom = () => {
@@ -23,23 +22,30 @@ const getNumRandom = () => {
     return Math.floor(Math.random() * (max - min) + min);
   }
 
-const obtenerPokePropio = ()=>{
+  const obtenerPokePropio = () => {
+    const num = parseInt(input.value); // Obtener el valor del input y convertirlo a entero
 
-    const num=input.value;
-    alert(num);
-
-
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`).then((res)=>{
-        console.log(res);
-        return res.data
-    }).then((res)=>{
-        console.log(res);
-        imgPoke.src = res.sprites.back_default;
-        pokeTipo.innerHTML = res.types[0].type.name;
-        namePoke.innerHTML = res.name;
-        pokeAtaque.innerHTML = res.stats[0].base_stat;
-    })
-}
+    // Verificar si el valor es un número y está dentro del rango permitido (1-1000)
+    if (!isNaN(num) && num >= 1 && num <= 1000) {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`)
+            .then((res) => {
+                console.log(res);
+                return res.data;
+            })
+            .then((res) => {
+                console.log(res);
+                imgPoke.src = res.sprites.back_default;
+                pokeTipo.innerHTML = res.types[0].type.name;
+                namePoke.innerHTML = res.name;
+                pokeAtaque.innerHTML = res.stats[0].base_stat;
+            });
+    } else {
+        // Mostrar mensaje de error si el valor no es válido
+        alert("Por favor ingresa un número válido entre 1 y 1000.");
+        // También podrías limpiar el input aquí si lo consideras necesario
+        input.value = "";
+    }
+};
 
 
 const obtenerPokeRival = () =>{
@@ -60,12 +66,19 @@ const obtenerPokeRival = () =>{
     console.log(imgPoke2);
 }
 
-const combate = ()=>{
+const combate = () => {
+    const ataqueRival = parseInt(poke2Ataque.textContent);
+    const ataquePropio = parseInt(pokeAtaque.textContent);
+    if (ataquePropio > ataqueRival) {
+        alert("¡Ganaste el enfrentamiento!");
+    } else if (ataqueRival > ataquePropio) {
+        alert("¡Perdiste el enfrentamiento!");
+    } else {
+        alert("¡Es un empate!");
+    }
+};
 
-    const ataqueRival=parseInt(poke2Ataque.textContent);
-    const ataquePropio=parseInt(pokeAtaque.textContent);
-    console.log(ataquePropio);
-}
+
 
 window.addEventListener('load', obtenerPokeRival);
 btnElegir.addEventListener('click', obtenerPokePropio);
